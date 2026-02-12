a97d106ff77c390165058a952593697cf8ec965c
Pedro <pedrofbks@gmail.com>
2026-02-12 13:44:35 -0300

    commit classes de security

---
+++ src/main/java/com/javapr/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.javapr.usuario.repository;
+
+import com.projetospring.java.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
