e7844f4aa22cd44020506c8d927f05b7d7af0435
Pedro <pedrofbks@gmail.com>
2026-03-05 16:24:41 -0300

    finalizacao codigo

--- src/main/java/com/javapr/usuario/business/ViaCepService.java
+++ src/main/java/com/javapr/usuario/business/ViaCepService.java
@@ -2,10 +2,11 @@ package com.javapr.usuario.business;
 
 import com.javapr.usuario.infrastructure.clients.ViaCepClient;
 import com.javapr.usuario.infrastructure.clients.ViaCepDTO;
+import com.javapr.usuario.infrastructure.exeptcions.IllegalArgumentException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.Objects;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
@@ -14,21 +15,24 @@ public class ViaCepService {
     private final ViaCepClient client;
 
     public ViaCepDTO buscaDadosEndereco(String cep) {
-        return client.buscaDadosEndereco(cep);
+        String cepValido = processarCep(cep);
+        return client.buscaDadosEndereco(cepValido);
     }
 
     private String processarCep(String cep) {
-        String cepFormatado = cep.replace("-", "").
-                replace(".", "").replace(" ", "");
-
-
-        if (cepFormatado.matches("\\d+")
-                || !Objects.equals(cepFormatado.length(),  8)) {
-            throw new IllegalArgumentException("O cep contém caracteres inválidos, favor verificar");
-
-
+        if (cep == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "O CEP não pode ser nulo.");
         }
+
+        String cepFormatado = cep.replace("-", "")
+                .replace(".", "")
+                .replace(" ", "");
+
+        if (!cepFormatado.matches("\\d+") || cepFormatado.length() != 8) {
+
+            throw new IllegalArgumentException("O cep contém caracteres inválidos ou tamanho incorreto, favor verificar");
+        }
+
         return cepFormatado;
     }
-
 }
